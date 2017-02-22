c2f339788ddeda6a8955082bbbd777cb161b01da
Ali <mr.alixb@gmail.com>
2017-02-22 23:35:04 +0100

    mework

---
+++ DTU/src/algorithms_and_datastructures/basic_concepts_ii/SearchingAndSorting.java
@@ -0,0 +1,111 @@
+package algorithms_and_datastructures.basic_concepts_ii;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class SearchingAndSorting {
+	
+	
+	// This method takes an array of integers as input parameters
+		// and a value val. It should return the index of val in the array.
+		// If val does not occur it should return -1.
+		private int find(int[] numbers, int val) {
+			// TODO: Implement this
+			
+			
+			
+			
+			
+			return val;
+			
+		}
+
+
+		// ##################################################
+		// # You do not need to modify anything below here. #
+		// ##################################################
+
+		public static void main(String[] args) throws IOException {
+			//new SearchingAndSorting().run();
+			int[] numbers = {2,7,1,8};
+			System.out.println(findSmallestMissingIntv2(numbers));
+		}
+
+		private void run() throws IOException {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			int[] numbers = readIntArray(in);
+			int[] queries = readIntArray(in);
+
+			for (int i = 0; i < queries.length; i++) {
+				int solution = find(numbers, queries[i]);
+				System.out.println(solution);
+			}
+		}
+
+		private int[] readIntArray(BufferedReader in) throws IOException {
+			int length = Integer.parseInt(in.readLine());
+			int[] array = new int[length];
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			for (int i = 0; i < length; i++) {
+				array[i] = Integer.parseInt(st.nextToken());
+			}
+			return array;
+		}
+		
+	
+		//Week 2, Mandatory ass. 1
+		public static int findSmallestMissingInt(int[] integers) {
+			int j = integers[0];
+			ArrayList<Integer> tempSmallestInts = new ArrayList<>();
+			
+			for(int i = 0; i < integers.length; i++) {
+				if(j == integers[i]) {
+					j++;
+					continue;
+				} else {
+					if(tempSmallestInts.isEmpty()) {
+						tempSmallestInts.add(j);
+					} else {
+						if(tempSmallestInts.get(0) > j) {
+							tempSmallestInts.remove(0);
+							tempSmallestInts.add(j);
+						}
+					}
+				}
+			}  
+			return tempSmallestInts.get(0);
+		    }
+		
+		public static int findSmallestMissingIntv2(int[] integers) {
+			int j = integers[0];
+			ArrayList<Integer> tempSmallestInts = new ArrayList<>();
+			int smallestNo = 0;
+			
+			for(int i = 0; i < integers.length; i++) {
+				if(j == integers[i]) {
+					j++;
+					continue;
+				} else {
+					tempSmallestInts.add(j);
+					j++;
+					i--;
+					}
+			}
+			
+			for(int k = 0; k < tempSmallestInts.size(); k++) {
+				smallestNo = tempSmallestInts.get(0);
+				if(tempSmallestInts.size() == 1) {
+					return smallestNo;
+				} else if (smallestNo > tempSmallestInts.get(k)) {
+					smallestNo = tempSmallestInts.get(k);
+				}
+			}
+				return smallestNo;
+		    }
+		}
+
+
