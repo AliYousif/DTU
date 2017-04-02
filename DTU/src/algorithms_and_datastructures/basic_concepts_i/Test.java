f036c90d38e9202995cec641f15a27f984b90995
Ali <mr.alixb@gmail.com>
2017-04-02 18:59:29 +0200

    gggggggg

---
+++ DTU/src/algorithms_and_datastructures/basic_concepts_i/Test.java
@@ -0,0 +1,35 @@
+package algorithms_and_datastructures.basic_concepts_i;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public void INTEGERANALYZER() {
+		
+		
+		int [] A = INTARRAY(READINT());
+		for(int i = 0; i < A.length;  i++) {
+			A[i] = READINT();
+		}
+		Arrays.sort(A);
+		for(int i = 0;  (A/2)) {
+			int s = 0;
+			
+			for(int j = 0; j < i; j++) {
+				s = s + A[ j] + A[n − j − 1]
+			}
+		PRINTINT(s);
+		}
+	}
+
+	private int[] INTARRAY(int readint) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private int READINT() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
